57ffa7df2a6901c4d372481105f9746f7ff67c34
Yoinier Hernandez Nieves <yoinier.hn@gmail.com>
2024-11-02 14:18:41 -0400

    [FIX] Fixed command for postfix in supervisord

--- config/startpostfix.sh
+++ config/startpostfix.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 trap "postfix stop" SIGINT
 trap "postfix stop" SIGTERM
 trap "postfix reload" SIGHUP
 
-# force new copy of hosts there (otherwise links could be outdated)
 cp /etc/hosts /var/spool/postfix/etc/hosts
 
-# start postfix
+mkdir -p /var/spool/postfix/pid
+
+postfix check
+
 postfix start
 
-# lets give postfix some time to start
 sleep 3
 
-# wait until postfix is dead (triggered by trap)
 while kill -0 "`cat /var/spool/postfix/pid/master.pid`"; do
   sleep 5
 done
